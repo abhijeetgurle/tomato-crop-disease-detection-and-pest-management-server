a00342f87a634c33ae714fb989e3632d8f99d55b
Sankalp <sankalpbarathe@gmail.com>
2019-04-01 11:32:40 +0530

    Third commit

--- disease/views.py
+++ disease/views.py
@@ -9,25 +9,25 @@ from django.core.files import File
 from PIL import Image
 import numpy as np
 from django.http import JsonResponse
-import torch.utils.model_zoo as model_zoo
-from torchvision.datasets import ImageFolder
-from torchvision.transforms import ToTensor
-from torch.utils.data import DataLoader
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-from torch.autograd import Variable
-import torchvision
-from torchvision import datasets, models, transforms
+#import torch.utils.model_zoo as model_zoo
+#from torchvision.datasets import ImageFolder
+#from torchvision.transforms import ToTensor
+#from torch.utils.data import DataLoader
+#import torch
+#import torch.nn as nn
+#import torch.optim as optim
+#from torch.optim import lr_scheduler
+#from torch.autograd import Variable
+#import torchvision
+#from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
 import time
 import os
-import torch.nn.functional as F
-import torch.optim as optim
+#import torch.nn.functional as F
+#import torch.optim as optim
 import json
 from django.conf import settings
-from .torchmodels import *
+#from .torchmodels import *
 from datetime import datetime
 from .models import *
 from sklearn.neighbors import KNeighborsRegressor
@@ -63,23 +63,23 @@ with open(os.path.join(settings.BASE_DIR, "plant_labels.json")) as f:
 # net = alexnet(True)
 # finetune = finenet(True)
 
-is_cuda = torch.cuda.is_available()	
-if is_cuda:
-	finetune = finetune.cuda()
-	net = net.cuda()
-	finetune2 = finetune2.cuda()
-	net2 = net2.cuda()
+#is_cuda = torch.cuda.is_available()	
+#if is_cuda:
+#	finetune = finetune.cuda()
+#	net = net.cuda()
+#	finetune2 = finetune2.cuda()
+#	net2 = net2.cuda()
 
 # Data transformation for forward pass
-data_transforms = {
-
-	'val': transforms.Compose([
-			transforms.CenterCrop(224),
-			transforms.ToTensor(),
-			transforms.Normalize(mean=[0.485, 0.456, 0.406],
-								 std=[0.229, 0.224, 0.225])
-	])
-}
+#data_transforms = {
+#
+#	'val': transforms.Compose([
+#			transforms.CenterCrop(224),
+#			transforms.ToTensor(),
+#			transforms.Normalize(mean=[0.485, 0.456, 0.406],
+#								 std=[0.229, 0.224, 0.225])
+#	])
+#}
 
 loaded_model= load_model('Tomato_diseases.h5')
 loaded_model._make_predict_function()
@@ -249,58 +249,58 @@ def get_normalized_probability(lat, lon, locs, probs, SCALE=1e-3):
 	return (reg.predict(X_test)[0])*np.exp(-SCALE*(mean_dist))
 
 
-def predict_without_name(image_arr):
-	print("Without name \n\n\n")
-	input_image = (data_transforms['val'](image_arr)).cuda()
-	outputs = finetune(net(Variable(input_image.unsqueeze(0))))
+#def predict_without_name(image_arr):
+#	print("Without name \n\n\n")
+#	input_image = (data_transforms['val'](image_arr)).cuda()
+#	outputs = finetune(net(Variable(input_image.unsqueeze(0))))
+#
+#	# Converting the numerical outputs to probabilities
+#	soft = nn.Softmax()
+#	prob = soft(outputs)
 
-	# Converting the numerical outputs to probabilities
-	soft = nn.Softmax()
-	prob = soft(outputs)
-
-	# Number of results
-	num_predictions = 5
-	val,ind = torch.topk(prob, num_predictions)
-	# print val, ind, prob.sum()
-
-	# Fetch from json
-	final = []
-	for i in xrange(num_predictions):
-		final.append({
-			'rank'			:	i,
-			'category'		: 	ind[0].data[i],
-			'category_name'	:  	idx_to_labels[str(ind[0].data[i])],
-			'prob'			:	val[0].data[i],
-		})
-
-	return final
-
-def predict_with_name(image_arr, crop_idx):
-	print("With name \n\n\n")
-	print("Crop index: ", crop_idx)
-
-	plant_label = Variable(torch.LongTensor(np.array([crop_idx])).cuda())
-	input_image = (data_transforms['val'](image_arr)).cuda()
-	o1 = net2(Variable(input_image.unsqueeze(0)))
-	outputs = finetune2(o1, plant_label)
-
-	# Converting the numerical outputs to probabilities
-	soft = nn.Softmax()
-	prob = soft(outputs)
-
-	# Number of results
-	num_predictions = 5
-	val,ind = torch.topk(prob, num_predictions)
-	# print val, ind, prob.sum()
-
-	# Fetch from json
-	final = []
-	for i in xrange(num_predictions):
-		final.append({
-			'rank'			:	i,
-			'category'		: 	ind[0].data[i],
-			'category_name'	:  	idx_to_labels_v2[str(ind[0].data[i])],
-			'prob'			:	val[0].data[i],
-		})
-
-	return final
+#	# Number of results
+#	num_predictions = 5
+#	val,ind = torch.topk(prob, num_predictions)
+#	# print val, ind, prob.sum()
+#
+#	# Fetch from json
+#	final = []
+#	for i in xrange(num_predictions):
+#		final.append({
+#			'rank'			:	i,
+#			'category'		: 	ind[0].data[i],
+#			'category_name'	:  	idx_to_labels[str(ind[0].data[i])],
+#			'prob'			:	val[0].data[i],
+#		})
+#
+#	return final
+#
+#def predict_with_name(image_arr, crop_idx):
+#	print("With name \n\n\n")
+#	print("Crop index: ", crop_idx)
+#
+#	plant_label = Variable(torch.LongTensor(np.array([crop_idx])).cuda())
+#	input_image = (data_transforms['val'](image_arr)).cuda()
+#	o1 = net2(Variable(input_image.unsqueeze(0)))
+#	outputs = finetune2(o1, plant_label)
+#
+#	# Converting the numerical outputs to probabilities
+#	soft = nn.Softmax()
+#	prob = soft(outputs)
+#
+#	# Number of results
+#	num_predictions = 5
+#	val,ind = torch.topk(prob, num_predictions)
+#	# print val, ind, prob.sum()
+#
+#	# Fetch from json
+#	final = []
+#	for i in xrange(num_predictions):
+#		final.append({
+#			'rank'			:	i,
+#			'category'		: 	ind[0].data[i],
+#			'category_name'	:  	idx_to_labels_v2[str(ind[0].data[i])],
+#			'prob'			:	val[0].data[i],
+#		})
+#
+#	return final
